13baf5f6a1735c2cd321f875b1cf663fa1453845
Alex Cowell <alxcwll@gmail.com>
2012-02-18 20:58:11 +0000

    Fixed the results message array so that the messages were correctly stored and freed. Cleaned up code a little.

--- src/micro_unit_test.c
+++ src/micro_unit_test.c
@@ -39,7 +39,7 @@ static int test_equality() {
 
 static char* test_new_truth_checks() {
     assertTrue(1);
-    assertFalse(1);
+    assertFalse(0);
 
     return 0;
 }
@@ -47,7 +47,7 @@ static char* test_new_truth_checks() {
 static char* test_another_dummy() {
     int i = 3;
     assertTrue(i == 3);
-    assertFalse(i == 3);
+    assertFalse(i == 2);
 
     return 0;
 }
@@ -59,46 +59,38 @@ static int all_tests() {
     //       Adding tests to the test suite will keep track of the number of
     //       tests. Each test can only have up to 1 error message.
     int num_tests = 3;
-    char results[num_tests][80];
-    int failed = 0;
+    char* results[num_tests];
+    int hasFailed = 0;
 
     int i;
     for (i = 0; i < num_tests; i++) {
-        strcpy(results[i], "");
+        results[i] = 0;
     }
 
     if (test_new_truth_checks() != 0) {
-        strcpy(results[0], test_new_truth_checks());
-        failed = 1;
+        results[0] = test_new_truth_checks();
+        hasFailed = 1;
     }
 
     if (test_another_dummy() != 0) {
-        strcpy(results[1], test_another_dummy());
-        failed = 1;
+        results[1] = test_another_dummy();
+        hasFailed = 1;
     }
 
     if (test_truth_checks() != 0) {
-        strcpy(results[2], test_truth_checks());
-        failed = 1;
+        results[2] = test_truth_checks();
+        hasFailed = 1;
     }
 
     for (i = 0; i < num_tests; i++) {
-        printf("%s", results[i]);
+        if (results[i] != 0) printf("%s", results[i]);
     }
 
-
-    // TODO: free() the result messages.
-    // ATM this is impossible since the messages are being malloc'd THEN copied
-    // into the results array. So...
-    // TODO: Change the results array to be an array of char pointers and assign
-    // memory addresses instead.
-    /*
     for (i = 0; i < num_tests; i++) {
         if (results[i] != 0) free(results[i]);
     }
-    */
 
-    return failed;
+    return hasFailed;
 }
 
 int main(int argc, char** argv) {
